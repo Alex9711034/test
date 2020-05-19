c5b89baabdffee9bc9b5c4155ed5d52e2626571e
Alex <alskobov@gmail.com>
2020-05-19 17:30:54 +0300

    Первый комит

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/*
+var leftBorderWidht = 1;
+let second = 2;
+const pi = 3.14;
+
+var number = 5; // Number
+var string = 'Hello'; // String
+var sym = Symbol(); // Symbol
+var boolean = true; // true
+var type5 = null;  // ссылка на не существующую вещь
+var type6; // undefined // объект уже существует, но значение не имеет
+var obj = {};
+
+
+
+console.log(4/0);
+console.log(4*'stringg');
+console.log(type6);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone.isMarried);
+
+let arr = ['plum.jpg','gfd.jpg','apple.jpg'];
+
+console.log(arr[0]);
+*/
+/*
+// -to String 
+console.log(typeof(String(4))); //(1) String() превращает в строку
+console.log(typeof(5 + 'ww'));  //(2) конактенация + превращает в строку
+console.log('str' + true); // true превращается с строку
+
+// -to Number
+console.log(typeof(Number('5'))); //(1) Number() превращает в число
+console.log(5 + +'5');        //(2) + превращ. число
+console.log(parseInt('15px')); //(3) отделит только цисло
+*/
+// 0, '', null, undefined, NaN - false
+
+// let switcher = null;
+// if (switcher) console.log('working..'); 
+// switcher = 1;
+// if (switcher) console.log('working..');
+
+// let ans = prompt("число", "");
+// console.log(parseInt(ans));
+
+// let inrc = 10,
+//     decr = 10;
+
+// console.log(inrc++);
+// console.log(decr--);
+// console.log(inrc);
+// console.log(decr);
+
+// console.log(2 + '2');
+// console.log(2 + +'2');
+
+// let isChecken = false,
+//     isClose = false;
+
+// console.log(isChecken || !!isClose); // ! инвертирует ложь в правду
+//                                      // !! обратная инверсия в ложь
