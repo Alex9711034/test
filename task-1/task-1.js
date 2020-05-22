5b5ee4eef1d58a166550f12419420d1a314afd83
Alex <alskobov@gmail.com>
2020-05-22 13:41:20 +0300

    02-009-z-3.1

--- task-1/task-1.js
+++ task-1/task-1.js
@@ -12,14 +12,32 @@ let appData = {
     savings: false,
 };
 
-let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
-let expenseItemSum = +prompt("Во сколько обойдется?");
-appData.expenses[expenseItem] = expenseItemSum;
-console.log(appData.expenses);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную ст. расходов в этом месяце"),
+        b = +prompt("Во сколько обойдется?");
 
-expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
-expenseItemSum = +prompt("Во сколько обойдется?");
-appData.expenses[expenseItem] = expenseItemSum;
-console.log(appData.expenses);
+    if ((typeof(a)) === 'string' && (typeof(a)) != null 
+        && (typeof(b)) != null && a != '' && b != ''
+        && a.length < 50 ) {
+    appData.expenses[a] = b;
+    console.log(appData.expenses);
+    } else {
+        --i;
+        continue;
+    }
+}
 
-alert("Ваш расчетный заработок в день: " + (money / 30));
+appData.moneyPerDay = appData.incom / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay <= 700) {
+    console.log("Минимальный уровень достатка")
+} else if (appData.moneyPerDay >= 701 
+            && appData.moneyPerDay <= 3000) {
+             console.log("Средний уровень достатка");   
+            }
+  else if (appData.moneyPerDay > 3000) {
+      console.log("Высокий уровень достатка");
+  } else {
+      console.log("Произошла ошибка");
+  }
