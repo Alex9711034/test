b7a5c5a9815983916dade04bf183add24a2ef122
Alex <alskobov@gmail.com>
2020-05-22 13:42:29 +0300

    02-009-z-3.1.1

---
+++ fask-2/task-2.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let num = 50;
+for (let i = num; i < 55; i++) {
+    console.log(i);
+}
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+num = 50;
+do {
+    console.log(num);
+    num++;
+} while (num < 55);  
